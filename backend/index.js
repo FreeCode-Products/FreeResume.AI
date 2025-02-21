cce07afec943eb5c930a93f3bfb6a6070617cb59
kavirasan <kavirasan1998@gmail.com>
2025-02-21 13:28:19 +0530

    Project Initialised

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+// Define a port number
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Basic Route
+app.get("/", (req, res) => {
+    res.send("Hello, Express.js Server is Running! 🚀");
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
